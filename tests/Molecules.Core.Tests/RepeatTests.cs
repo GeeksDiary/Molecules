24ea8f334c9aa4cf3ba2232c95d27812270710b5
Buddhike de Silva <buddhike.desilva@geeksdiary.com>
2015-06-26 16:01:48 +1000

    Simplified repeat test.

--- tests/Molecules.Core.Tests/RepeatTests.cs
+++ tests/Molecules.Core.Tests/RepeatTests.cs
@@ -7,11 +7,8 @@ namespace Molecules.Core.Tests
         [Fact]
         public async void ShouldRepeatSpecifiedNumberOfTimes()
         {
-            var a = Atom.Of((int i) => i).Repeat(3);
-
-            var result = await a.Charge(1);
-
-            Assert.Equal(new[] {1, 1, 1}, result);
+            Assert.Equal(new[] {1, 1, 1}, 
+                await Atom.Of((int i) => i).Repeat(3).Charge(1));
         } 
     }
 }
