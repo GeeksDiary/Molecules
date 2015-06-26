24bb6dae49c8a0b93a40a027fd3851eec8037f21
Buddhike de Silva <buddhike.desilva@geeksdiary.com>
2015-06-26 12:36:53 +1000

    Simplified Repeat test.

--- tests/Dependable.Core.Tests/RepeatTests.cs
+++ tests/Dependable.Core.Tests/RepeatTests.cs
@@ -1,17 +1,13 @@
-﻿using NSubstitute;
-using Xunit;
+﻿using Xunit;
 
 namespace Dependable.Core.Tests
 {
     public class RepeatTests
     {
-        readonly IApi _api = Substitute.For<IApi>();
-
         [Fact]
         public async void ShouldRepeatSpecifiedNumberOfTimes()
         {
-            _api.Call(1).Returns(1);
-            var a = Atom.Of((int i) => _api.Call(i)).Repeat(3);
+            var a = Atom.Of((int i) => i).Repeat(3);
 
             var result = await a.Charge(1);
 
